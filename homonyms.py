1d09330188ded0e137ef23b73955860f2a77a093
Esta Prangel <esta.prangel@eki.ee>
2023-10-25 16:25:39 +0300

    Homonyms

--- homonyms.py
+++ homonyms.py
@@ -1,7 +1,7 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, NavigableString
 import re
 
-
+# Your HTML file path
 html_file_path = 'failid/sõnastikud/keelevara/et-en_silvet/HTML.html'
 
 with open(html_file_path, 'r', encoding='utf-8') as f:
@@ -9,23 +9,48 @@ with open(html_file_path, 'r', encoding='utf-8') as f:
 
 soup = BeautifulSoup(html_data, 'html.parser')
 
+# Regex pattern to match Roman numerals
 roman_numeral_pattern = re.compile(r'<big><b>(I|II|III|IV|V|VI|VII)</b></big>')
+empty_brackets_pattern = re.compile(r'\[\s*\]\s*')
 
-elements_with_mrks = soup.find_all(class_='mrks')
+extracted_content = []
 
-filtered_elements = []
+for element in soup.find_all(class_='mrks'):
+    content_list = []
+    term = element.get_text()
 
-for element in elements_with_mrks:
-    next_sibling = element.find_next_sibling()
+    sibling = element.next_sibling
+    while sibling:
+        if isinstance(sibling, Tag):
+            if sibling.has_attr('class') and 'mrks' in sibling['class']:
+                break
+            content_list.append(str(sibling).strip())
+        elif isinstance(sibling, NavigableString):
+            content_list.append(str(sibling).strip())
 
-    content_between_mrks = str(element)
+        sibling = sibling.next_sibling if sibling else None
 
-    while next_sibling and not next_sibling.has_attr('class'):
-        content_between_mrks += str(next_sibling)
-        next_sibling = next_sibling.find_next_sibling()
+    content_between_mrks = ' '.join(content_list).replace('\n', ' ').replace('&nbsp;', ' ')
+
+    soup_content = BeautifulSoup(content_between_mrks, 'html.parser')
 
     if roman_numeral_pattern.search(content_between_mrks):
-        filtered_elements.append(element)
+        for tag_type in ['sl', 'term', 'stiil']:
+            for tag in soup_content.find_all('i', {'class': tag_type}):
+                tag.decompose()
+        for tag_type in ['ipa']:
+            for tag in soup_content.find_all('span', {'class': tag_type}):
+                tag.decompose()
 
-for element in filtered_elements:
-    print(element)
+        content_between_mrks = str(soup_content).strip()
+
+        content_between_mrks = roman_numeral_pattern.sub('', content_between_mrks)
+        content_between_mrks = empty_brackets_pattern.sub('', content_between_mrks)
+        extracted_content.append([term, content_between_mrks.strip()])
+
+output_file_path = 'output/homonyms_silvet.txt'
+
+# Open the file and write the extracted content
+with open(output_file_path, 'w', encoding='utf-8') as f:
+    for content in extracted_content:
+        f.write(str(content) + '\n')
